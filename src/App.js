ec5466f99832d893a0b2cf9d2e6b46953f086aa7
bolatah <bolatah@outlook.com>
2022-05-01 14:49:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  FormattedMessage,
+  FormattedDate,
+  FormattedNumber,
+  FormattedPlural,
+  FormattedTime,
+} from "react-intl";
+import { useContext } from "react";
+import { Context } from "./components/Wrapper";
 
-function App() {
+function App(props) {
+  const context = useContext(Context);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <select value={context.locale} onChange={context.selectLanguage}>
+          <option value="en">English</option>
+          <option value="fr">French</option>
+          <option value="ar">Arabic</option>
+        </select>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage
+            id="app.header"
+            defaultMessage="Edit <code>{fileName}</code> js save to reload."
+            values={{
+              fileName: "src/App.js",
+              code: (word) => <code>{word}</code>,
+            }}
+          />
         </p>
         <a
           className="App-link"
@@ -15,8 +37,44 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FormattedMessage id="app.content" defaultMessage="Learn React" />
         </a>
+        <FormattedMessage
+          id="app.channel.plug"
+          defaultMessage="Tutorial brought to you by Lokalise"
+          values={{ blogName: "Lokalise" }}
+        />
+        <FormattedPlural
+          id="app.plural"
+          defaultMessage="{amount, plural, =0 {no languages} one {# one language} few {# several languages} many {# lots of languages} other {# wrong fromat}}"
+          values={{ amount: 90 }}
+        />
+        <br />
+        <FormattedDate
+          value={props.date}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
+        <br />
+        <FormattedNumber
+          value={20.42}
+          //eslint-disable-next-line
+          style="currency"
+          currencyDisplay="symbol"
+          currency="USD"
+        />
+        <br />
+        <FormattedNumber value={10000} />
+        <br />
+        <FormattedTime
+          value={new Date()}
+          hour="numeric"
+          minute="numeric"
+          second="numeric"
+          timeZoneName="long"
+        />
       </header>
     </div>
   );
